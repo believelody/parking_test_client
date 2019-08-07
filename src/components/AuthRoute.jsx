f93a3505bf6c881fe1f60afeede4adf47bd21db8
believelody <believelody@gmail.com>
2019-08-07 10:39:45 +0200

    Change jwt-decode for jsonwebtoken librairy

--- src/components/AuthRoute.jsx
+++ src/components/AuthRoute.jsx
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from 'react'
-import { Route, Redirect } from 'react-router-dom'
-import jwt_decode from 'jwt-decode'
-import styled from 'styled-components'
-import { useAppHook } from '../contexts'
-import Nav from '../components/Nav'
-import { DISCONNECTED, AUTH_SUCCESS } from '../reducers/userReducer';
-import setAuth from '../utils/setAuth';
-import devices from '../utils/devices';
+import React, { useEffect, useState } from "react";
+import { Route, Redirect } from "react-router-dom";
+import jwt from "jsonwebtoken";
+import styled from "styled-components";
+import { useAppHook } from "../contexts";
+import Nav from "../components/Nav";
+import { DISCONNECTED, AUTH_SUCCESS } from "../reducers/userReducer";
+import setAuth from "../utils/setAuth";
+import devices from "../utils/devices";
 
 const Container = styled.div`
   width: 60%;
@@ -23,47 +23,45 @@ const Container = styled.div`
   @media ${devices.mobileL} {
     width: 100%;
   }
-`
+`;
 
 const AuthRoute = ({ component: Component, ...rest }) => {
-  const { useUser } = useAppHook()
-  const [{ isConnected }, dispatch] = useUser
+  const { useUser } = useAppHook();
+  const [{ isConnected }, dispatch] = useUser;
 
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     if (localStorage.token && !isConnected) {
-      setAuth(localStorage.token)
-      const decoded = jwt_decode(localStorage.token)
-      const currentTime = Date.now() / 1000
+      setAuth(localStorage.token);
+      const decoded = jwt.verify(localStorage.token, "yeswecan");
+      const currentTime = Date.now() / 1000;
       if (decoded.exp < currentTime) {
-        dispatch({ type: DISCONNECTED })
-      }
-      else {
-        dispatch({ type: AUTH_SUCCESS, payload: decoded })
+        dispatch({ type: DISCONNECTED });
+      } else {
+        dispatch({ type: AUTH_SUCCESS, payload: decoded });
       }
     }
-    setLoading(false)
-  }, [dispatch])
+    setLoading(false);
+  }, [dispatch]);
 
   return (
     <Route
       {...rest}
       render={props => (
         <React.Fragment>
-        {loading && <h2>Loading...</h2>}
-        {!loading &&!isConnected && <Redirect to='/login' />}
-        {
-          !loading && isConnected &&
-          <Container>
-            <Nav />
-            <Component {...props} />
-          </Container>
-        }
+          {loading && <h2>Loading...</h2>}
+          {!loading && !isConnected && <Redirect to="/login" />}
+          {!loading && isConnected && (
+            <Container>
+              <Nav />
+              <Component {...props} />
+            </Container>
+          )}
         </React.Fragment>
       )}
     />
-  )
-}
+  );
+};
 
-export default AuthRoute
+export default AuthRoute;
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,12 +1,16 @@
-import React, { useState, useEffect } from 'react'
-import { Redirect, Link } from 'react-router-dom'
-import jwt_decode from 'jwt-decode'
-import styled from 'styled-components'
-import { useAppHook } from '../contexts'
-import api from '../api'
-import devices from '../utils/devices'
-import { AUTH_FAILED, AUTH_SUCCESS, DISCONNECTED } from '../reducers/userReducer';
-import setAuth from '../utils/setAuth';
+import React, { useState, useEffect } from "react";
+import { Redirect, Link } from "react-router-dom";
+import jwt from "jsonwebtoken";
+import styled from "styled-components";
+import { useAppHook } from "../contexts";
+import api from "../api";
+import devices from "../utils/devices";
+import {
+  AUTH_FAILED,
+  AUTH_SUCCESS,
+  DISCONNECTED
+} from "../reducers/userReducer";
+import setAuth from "../utils/setAuth";
 
 const LoginStyle = styled.div`
   width: 100%;
@@ -37,41 +41,41 @@ const LoginStyle = styled.div`
   @media ${devices.mobileL} {
     width: 100%;
   }
-`
+`;
 
 const Login = () => {
-  const { useUser } = useAppHook()
-  const [{errors, isConnected}, dispatch] = useUser
+  const { useUser } = useAppHook();
+  const [{ errors, isConnected }, dispatch] = useUser;
 
-  const [email, setEmail] = useState(null)
-  const [password, setPassword] = useState(null)
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
 
   const handleSubmit = async e => {
-    e.preventDefault()
+    e.preventDefault();
     try {
-      let res = await api.user.login(email, password)
-      const { token } = res.data
-      setEmail('')
-      setPassword('')
-      localStorage.setItem('token', token)
-      setAuth(token)
-      const decoded = jwt_decode(token)
-      dispatch({ type: AUTH_SUCCESS, payload: decoded })
+      let res = await api.user.login(email, password);
+      const { token } = res.data;
+      setEmail("");
+      setPassword("");
+      localStorage.setItem("token", token);
+      setAuth(token);
+      const decoded = jwt.verify(token, "yeswecan");
+      dispatch({ type: AUTH_SUCCESS, payload: decoded });
     } catch (error) {
-      const { email, password } = error.response.data
-      setEmail(null)
-      setPassword(null)
-      if (email) dispatch({ type: AUTH_FAILED, payload: email})     
-      if (password) dispatch({ type: AUTH_FAILED, payload: password})     
+      const { email, password } = error.response.data;
+      setEmail(null);
+      setPassword(null);
+      if (email) dispatch({ type: AUTH_FAILED, payload: email });
+      if (password) dispatch({ type: AUTH_FAILED, payload: password });
     }
-  }
+  };
 
   useEffect(() => {
-    console.log(errors)
-    if (errors && errors.email) alert(errors.email)
-    if (errors && errors.password) alert(errors.password)
-    return () => dispatch({ type: DISCONNECTED })
-  }, [errors])
+    console.log(errors);
+    if (errors && errors.email) alert(errors.email);
+    if (errors && errors.password) alert(errors.password);
+    return () => dispatch({ type: DISCONNECTED });
+  }, [errors]);
 
   // useEffect(() => {
   //   if (localStorage.token) {
@@ -79,19 +83,30 @@ const Login = () => {
   //   }
   // }, [localStorage.token])
 
-  return !localStorage.token ?
+  return !localStorage.token ? (
     <LoginStyle>
-      <div className='login-form'>
+      <div className="login-form">
         <form onSubmit={handleSubmit}>
-          <input required type='email' placeholder='enter your email' onChange={e => setEmail(e.target.value)} />
-          <input required type='password' placeholder='enter your password' onChange={e => setPassword(e.target.value)} />
-          <input type='submit' value='Login' />
+          <input
+            required
+            type="email"
+            placeholder="enter your email"
+            onChange={e => setEmail(e.target.value)}
+          />
+          <input
+            required
+            type="password"
+            placeholder="enter your password"
+            onChange={e => setPassword(e.target.value)}
+          />
+          <input type="submit" value="Login" />
         </form>
-        <Link to='/register'>Create an account</Link>
+        <Link to="/register">Create an account</Link>
       </div>
     </LoginStyle>
-    :
-    <Redirect to='/' />
-}
+  ) : (
+    <Redirect to="/" />
+  );
+};
 
-export default Login
+export default Login;
